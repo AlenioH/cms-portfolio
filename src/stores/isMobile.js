69c28ddf2b9cce47311f499399c9582e4d9cbbfc
Alena Hasslacher <alena.hasslacher@gmail.com>
2024-12-07 15:25:41 +0100

    Add smallScreen store

--- src/stores/isMobile.js
+++ src/stores/isMobile.js
@@ -1,17 +1,24 @@
 import { writable } from 'svelte/store';
 
-//create a writable store
 export const isMobile = writable(false);
 
-//function to update the store based on screen width
 function updateIsMobile() {
-  console.log('resize');
   const width = window.innerWidth;
-  isMobile.set(width < 768); // mobile if width is less than 768px
+  isMobile.set(width < 768);
 }
 
-//listen to resize events and update the store
+
+export const isSmallScreen = writable(false);
+
+function updateIsSmallScreen() {
+  const width = window.innerWidth;
+  isSmallScreen.set(width < 1080);
+}
+
+
 if (typeof window !== 'undefined') { //ensure this only runs in the browser
   window.addEventListener('resize', updateIsMobile);
-  updateIsMobile(); //set the initial value
+  window.addEventListener('resize', updateIsSmallScreen);
+  updateIsMobile();
+  updateIsSmallScreen();
 }
