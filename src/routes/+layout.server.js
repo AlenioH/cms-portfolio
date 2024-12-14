95289671732626ff8dec145eb2b4c2368e4631fe
Alena Hasslacher <alena.hasslacher@gmail.com>
2024-12-14 17:27:45 +0100

    populate project images

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -4,7 +4,7 @@ const API_URL = import.meta.env.VITE_API_URL;
 export async function load() {
   const person = await axios.get(`${API_URL}/api/person?populate=photo&populate=logo`);
 
-  const projects = await axios.get(`${API_URL}/api/projects`);
+  const projects = await axios.get(`${API_URL}/api/projects?populate=images`);
 
   return {
     person: person.data.data,
